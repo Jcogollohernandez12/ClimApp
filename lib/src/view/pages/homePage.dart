06a64de1aaa2153b5d60423abc1afc6ece9ef639
Jose Andres C H <josandres123@gmail.com>
2022-03-31 13:05:52 -0500

    first commit

--- lib/src/view/pages/homePage.dart
+++ lib/src/view/pages/homePage.dart
@@ -38,7 +38,6 @@ class _HomePageState extends State<HomePage> {
                 setState(() {
                   location = res;
                   isData = true;
-                  //
                 });
               }
             },
